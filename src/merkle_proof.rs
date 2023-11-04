2734bbb7add3b766308c8d0096d3a2787e256e9a
bob <gunjan.cn@gmail.com>
2023-11-04 14:59:41 +0530

    shrink to single column advice

--- src/merkle_proof.rs
+++ src/merkle_proof.rs
@@ -5,9 +5,7 @@ use std::marker::PhantomData;
 
 #[derive(Debug, Clone)]
 struct MerkleConfig {
-    pub col_a: Column<Advice>,
-    pub col_b: Column<Advice>,
-    pub col_c: Column<Advice>,
+    pub advice: Column<Advice>,
     pub selector: Selector,
     pub instance: Column<Instance>,
 }
@@ -26,100 +24,77 @@ impl<F: FieldExt> MerkleChip<F> {
         }
     }
 
-    pub fn configure(meta: &mut ConstraintSystem<F>) -> MerkleConfig {
-        let col_a = meta.advice_column();
-        let col_b = meta.advice_column();
-        let col_c = meta.advice_column();
+    pub fn configure(
+        meta: &mut ConstraintSystem<F>,
+        advice: Column<Advice>,
+        instance: Column<Instance>,
+    ) -> MerkleConfig {
         let selector = meta.selector();
-        let instance = meta.instance_column();
 
-        meta.enable_equality(col_a);
-        meta.enable_equality(col_b);
-        meta.enable_equality(col_c);
+        meta.enable_equality(advice);
         meta.enable_equality(instance);
 
         meta.create_gate("add", |meta| {
             //
-            // col_a | col_b | col_c | selector
-            //   a      b        c       s
-            //
+            // advice | selector
+            //   a          s
+            //   b
+            //   c
+
             let s = meta.query_selector(selector);
-            let a = meta.query_advice(col_a, Rotation::cur());
-            let b = meta.query_advice(col_b, Rotation::cur());
-            let c = meta.query_advice(col_c, Rotation::cur());
+            let a = meta.query_advice(advice, Rotation::cur());
+            let b = meta.query_advice(advice, Rotation::next());
+            let c = meta.query_advice(advice, Rotation(2));
             vec![s * (a + b - c)]
         });
 
         MerkleConfig {
-            col_a,
-            col_b,
-            col_c,
+            advice,
             selector,
             instance,
         }
     }
 
-    #[allow(clippy::type_complexity)]
-    pub fn assign_first_row(
+    pub fn assign(
         &self,
         mut layouter: impl Layouter<F>,
-    ) -> Result<(AssignedCell<F, F>, AssignedCell<F, F>, AssignedCell<F, F>), Error> {
-        layouter.assign_region(
-            || "first row",
-            |mut region| {
-                self.config.selector.enable(&mut region, 0)?;
-
-                let a_cell = region.assign_advice_from_instance(
-                    || "f(0)",
-                    self.config.instance,
-                    0,
-                    self.config.col_a,
-                    0,
-                )?;
-
-                let b_cell = region.assign_advice_from_instance(
-                    || "f(1)",
-                    self.config.instance,
-                    1,
-                    self.config.col_b,
-                    0,
-                )?;
-
-                let c_cell = region.assign_advice(
-                    || "a + b",
-                    self.config.col_c,
-                    0,
-                    || a_cell.value().copied() + b_cell.value(),
-                )?;
-
-                Ok((a_cell, b_cell, c_cell))
-            },
-        )
-    }
-
-    pub fn assign_row(
-        &self,
-        mut layouter: impl Layouter<F>,
-        prev_b: &AssignedCell<F, F>,
-        prev_c: &AssignedCell<F, F>,
+        nrows: usize,
     ) -> Result<AssignedCell<F, F>, Error> {
         layouter.assign_region(
-            || "next row",
+            || "entire merkle table",
             |mut region| {
                 self.config.selector.enable(&mut region, 0)?;
+                self.config.selector.enable(&mut region, 1)?;
 
-                // Copy the value from b & c in previous row to a & b in current row
-                prev_b.copy_advice(|| "a", &mut region, self.config.col_a, 0)?;
-                prev_c.copy_advice(|| "b", &mut region, self.config.col_b, 0)?;
-
-                let c_cell = region.assign_advice(
-                    || "c",
-                    self.config.col_c,
+                let mut a_cell = region.assign_advice_from_instance(
+                    || "1",
+                    self.config.instance,
                     0,
-                    || prev_b.value().copied() + prev_c.value(),
+                    self.config.advice,
+                    0,
+                )?;
+                let mut b_cell = region.assign_advice_from_instance(
+                    || "1",
+                    self.config.instance,
+                    1,
+                    self.config.advice,
+                    1,
                 )?;
 
-                Ok(c_cell)
+                for row in 2..nrows {
+                    if row < nrows - 2 {
+                        let _ = self.config.selector.enable(&mut region, row);
+                    }
+                    let c_cell = region.assign_advice(
+                        || "advice",
+                        self.config.advice,
+                        row,
+                        || a_cell.value().copied() + b_cell.value(),
+                    )?;
+                    a_cell = b_cell;
+                    b_cell = c_cell;
+                }
+                Ok(b_cell)
             },
         )
     }
@@ -127,9 +102,10 @@ impl<F: FieldExt> MerkleChip<F> {
     pub fn expose_public(
         &self,
         mut layouter: impl Layouter<F>,
-        cell: &AssignedCell<F, F>,
-        row: usize,
+        cell: AssignedCell<F, F>,
+        row: usize, // row number in the instance column
     ) -> Result<(), Error> {
+        // cell should be equal to the value at row number row in instance column
         layouter.constrain_instance(cell.cell(), self.config.instance, row)
     }
 }
@@ -146,7 +122,9 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
     }
 
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        MerkleChip::configure(meta)
+        let advice = meta.advice_column();
+        let instance = meta.instance_column();
+        MerkleChip::configure(meta, advice, instance)
     }
 
     fn synthesize(
@@ -156,16 +134,8 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
     ) -> Result<(), Error> {
         let chip = MerkleChip::construct(config);
 
-        let (_, mut prev_b, mut prev_c) =
-            chip.assign_first_row(layouter.namespace(|| "first row"))?;
-
-        for _i in 3..10 {
-            let c_cell = chip.assign_row(layouter.namespace(|| "next row"), &prev_b, &prev_c)?;
-            prev_b = prev_c;
-            prev_c = c_cell;
-        }
-
-        chip.expose_public(layouter.namespace(|| "out"), &prev_c, 2)?;
+        let c = chip.assign(layouter.namespace(|| "entire table"), 10)?;
+        chip.expose_public(layouter.namespace(|| "out"), c, 2)?;
 
         Ok(())
     }
@@ -180,7 +150,7 @@ mod tests {
 
     #[test]
     fn merkle_example1() {
-        let k = 4;
+        let k = 5;
 
         let a = Fp::from(1); // F[0]
         let b = Fp::from(1); // F[1]
